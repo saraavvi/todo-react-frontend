a66587b032c431315dd7a08d93b7f26f0ff35809
Sara Viktorsson <saraviktorsson@hotmail.com>
2021-09-30 12:00:53 +0200

    :sparkles: fetch all lists from backend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import ListPage from "./pages/ListPage";
 import LoginPage from "./pages/LoginPage";
 function App() {
+  useEffect(() => {
+    getAllLists();
+  }, []);
+
+  function getAllLists() {
+    const url = "/api/lists";
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
   return (
     <div>
       <h1>Todo App!</h1>

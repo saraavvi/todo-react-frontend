c22d1c4b0266ecd6d508d35c2a16dce25218247f
Sara Viktorsson <saraviktorsson@hotmail.com>
2021-09-30 10:50:11 +0200

    :sparkles: create pages and their routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
-
+import { Switch, Route } from "react-router-dom";
+import ListPage from "./pages/ListPage";
+import LoginPage from "./pages/LoginPage";
 function App() {
   return (
     <div>
       <h1>Todo App!</h1>
+      <Switch>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/">
+          <ListPage />
+        </Route>
+      </Switch>
     </div>
   );
 }

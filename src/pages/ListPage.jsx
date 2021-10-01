d3a497f76fa706e93b050589cbfcd3bfe7a3b482
Sara Viktorsson <saraviktorsson@hotmail.com>
2021-10-01 14:04:41 +0200

    :sparkles: add form to create new lists

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,16 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ListContext } from "../contexts/ListContext";
 import List from "../components/List";
 
 export default function ListPage() {
   const { lists } = useContext(ListContext);
+  const [input, setInput] = useState("");
+
+  function handleOnChange(e) {
+    setInput(e.target.value);
+  }
+  function handleOnSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <div>
-      <h1>List Page</h1>
+      <form onSubmit={handleOnSubmit}>
+        <input
+          type="text"
+          placeholder="New list..."
+          onChange={handleOnChange}
+        />
+      </form>
       {lists && (
         <>
           {lists.map((list) => {
-            console.log(list);
             return <List key={list._id} list={list} />;
           })}
         </>

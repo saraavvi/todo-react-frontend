588066b88f900893cb8fa5c3de2ac2e8086aa817
Björn Tirsén <bjorntirsen@gmail.com>
2021-10-05 10:45:26 +0200

    :sparkles: Add sign up form to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
-import React from "react";
-import classes from "./Header.module.css";
+import React from 'react';
+import classes from './Header.module.css';
 import ModalButton from './ModalButton';
+import SignupForm from './SignupForm';
 
 export default function Header() {
   return (
-    <header className={classes["header"]}>
+    <header className={classes['header']}>
       <h1>header</h1>
-      <ModalButton buttonText={'Log In'}><p>Login form</p></ModalButton>
-      <ModalButton buttonText={'Sign Up'}><p>Sign form</p></ModalButton>
+      <ModalButton buttonText={'Log In'}>
+        <p>Login form</p>
+      </ModalButton>
+      <ModalButton buttonText={'Sign Up'}>
+        <SignupForm />
+      </ModalButton>
     </header>
   );
 }
---
+++ src/components/SignupForm.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+
+const SignupForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  });
+
+  const history = useHistory();
+
+  function handleOnChange(e) {
+    e.preventDefault();
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  const handlePost = (data) => {
+    return axios
+      .post('/api/users/signup', {
+        body: data,
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.token) {
+          localStorage.setItem('jwt', data.token);
+          history.push('/lists');
+        } else window.alert('Invalid email or password. Please try again.');
+      })
+      .catch((err) => console.log(err.response.data.message));
+  };
+
+  function handleOnSubmit(e) {
+    e.preventDefault();
+    const payload = {
+      name: formData.name,
+      email: formData.email,
+      password: formData.password,
+      passwordConfirm: formData.passwordConfirm,
+    };
+    handlePost(payload);
+  }
+
+  return (
+    <form onSubmit={handleOnSubmit}>
+      <div>
+        <h2>Sign Up:</h2>
+      </div>
+      <div>
+        <label htmlFor='signupName'>Name</label>
+        <input
+          type='name'
+          autoComplete='name'
+          id='signupName'
+          name='name'
+          value={formData.name}
+          onChange={handleOnChange}
+        />
+      </div>
+      <div>
+        <label htmlFor='signupEmail'>Email address</label>
+        <input
+          type='email'
+          autoComplete='email'
+          id='signupEmail'
+          name='email'
+          value={formData.email}
+          onChange={handleOnChange}
+        />
+      </div>
+      <div>
+        <label htmlFor='signupPassword'>Password</label>
+        <input
+          type='password'
+          autoComplete='password'
+          id='signupPassword'
+          name='password'
+          value={formData.password}
+          onChange={handleOnChange}
+        />
+      </div>
+      <div>
+        <label htmlFor='signupPasswordConfirm'>Confirm your password</label>
+        <input
+          type='passwordConfirm'
+          autoComplete='passwordConfirm'
+          id='signupPasswordConfirm'
+          name='passwordConfirm'
+          value={formData.passwordConfirm}
+          onChange={handleOnChange}
+        />
+      </div>
+      <div>
+        <input type='submit' value='Sign Up' />
+      </div>
+    </form>
+  );
+};
+
+export default SignupForm;

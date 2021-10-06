14996fc8d645cf16011c9557db42df7b9f0b3536
Björn Tirsén <bjorntirsen@gmail.com>
2021-10-06 09:07:16 +0200

    :sparkles: Enable updates with auth

--- src/api/Api.js
+++ src/api/Api.js
@@ -12,6 +12,7 @@ export const Api = {
       return console.log(err.response.data.message);
     }
   },
+
   createList: async (token, title) => {
     try {
       return axios.post(
@@ -27,32 +28,48 @@ export const Api = {
       return console.log(err.response.data.message);
     }
   },
-  updateList: (token, id, title, body) => {
-    return axios
-      .patch(`/api/lists/${id}`, {
-        title: title,
-        body: body,
-        headers: {
-          Authorization: `Bearer ${token}`,
+
+  updateList: async (token, id, title, body) => {
+    try {
+      return axios.patch(
+        `/api/lists/${id}`,
+        {
+          title: title,
+          body: body,
         },
-      })
-      .catch((err) => console.log(err.response.data.message));
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    } catch (err) {
+      return console.log(err.response.data.message);
+    }
   },
-  deleteList: (token, id) => {
-    return axios
-      .delete(`/api/lists/${id}`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-      .catch((err) => console.log(err.response.data.message));
+
+  deleteList: async (token, id) => {
+    try {
+      return axios
+        .delete(`/api/lists/${id}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+    } catch (err) {
+      return console.log(err.response.data.message);
+    }
   },
-  login: (data) => {
-    return axios
-      .post('http://localhost:3000/api/users/login', {
-        email: data.email,
-        password: data.password,
-      })
-      .catch((err) => console.log(err.response.data.message));
+
+  login: async (data) => {
+    try {
+      return axios
+        .post('http://localhost:3000/api/users/login', {
+          email: data.email,
+          password: data.password,
+        });
+    } catch (err) {
+      return console.log(err.response.data.message);
+    }
   },
 };
